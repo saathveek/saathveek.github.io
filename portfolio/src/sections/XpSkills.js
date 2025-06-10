c02752d791716edaa1da249f33f64acf6e40fd86
Saathveek <gsaathveek@gmail.com>
2025-06-09 22:24:34 -0500

    Fixed color theme of XpSkills section

--- portfolio/src/sections/XpSkills.js
+++ portfolio/src/sections/XpSkills.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 function XpSkills() {
     return (
-        <section id="XpSkills">
-            <div>
-                <h1>XpSkills</h1>
+        <section 
+            id="XpSkills" 
+            className="flex flex-col md:flex-row items-center justify-center gap-12 p-8 bg-background">
+            <div className="max-w-xl">
+                <h1 className="text-6xl font-semibold text-primary mb-6">
+                    Experience & Skills
+                </h1>
+
+                
+            </div>
+            <div className="w-96 h-[28rem]">
+                <img
+                    src="/PFP_Globe_Transparent.png"
+                    alt="Experience and Skills"
+                    className="w-full h-full object-cover rounded-xl border-primary"
+                />
             </div>
         </section>
     );
